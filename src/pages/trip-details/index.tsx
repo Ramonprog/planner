ad49212372b77d035bfa85afc21be6ebec6ef8a7
Ramonprog <programador.ramon@gmail.com>
2024-07-14 19:17:39 -0300

    feat: salvando

--- src/pages/trip-details/index.tsx
+++ src/pages/trip-details/index.tsx
@@ -1,10 +1,9 @@
-import { Calendar, MapPin, Settings2 } from "lucide-react";
+import { Calendar, MapPin, Plus, Settings2 } from "lucide-react";
 
 export function TripDetailsPage() {
   return (
     <div className="max-w-6xl px-6 py-10 mx-auto space-y-8">
-      <div className="px-4 h-16 rounded-xl bg-zinc-900 shadow-md flex items-center justify-between">
-
+      <header className="px-4 h-16 rounded-xl bg-zinc-900 shadow-md flex items-center justify-between">
         <div className="flex items-center gap-2">
           <MapPin size={20} className="text-zinc-400" />
           <span className="text-zinc-100 ">São Paulo, Brasil</span>
@@ -23,10 +22,28 @@ export function TripDetailsPage() {
             <Settings2 size={20} />
           </button>
         </div>
-      </div>
+      </header>
 
       <main className="flex gap-16">
-        <div className="flex-1"></div>
+        <div className="flex-1 space-y-6">
+          <div className="flex items-center justify-between">
+            <h2 className="text-3xl font-semibold">Atividades</h2>
+            <button className="bg-lime-300 text-lime-950  rounded-lg py-2 px-5 font-medium flex items-center gap-2 hover:bg-lime-400">
+              <Plus size={20} />
+              Cadastrar Atividade
+            </button>
+          </div>
+
+          <div className="space-y-8 ">
+            <div className="space-y-2.5">
+              <div className="flex gap-2 items-baseline">
+                <span className="text-xl font-semibold text-zinc-300">Dia 17</span>
+                <span className="text-xs text-zinc-500">Sábado</span>
+              </div>
+              <p className="text-zinc-500 text-sm">Nenhuma atividade cadastrada nessa data.</p>
+            </div>
+          </div>
+        </div>
         <div className="w-80"></div>
       </main>
     </div>
