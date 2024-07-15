81b92b5c8b55f5ad0ccdadebe9b3146ef809d9bf
Ramonprog <programador.ramon@gmail.com>
2024-07-15 20:32:44 -0300

    feat: finaliando layoute parte esqueda

--- src/pages/trip-details/index.tsx
+++ src/pages/trip-details/index.tsx
@@ -1,4 +1,4 @@
-import { Calendar, MapPin, Plus, Settings2 } from "lucide-react";
+import { Calendar, CircleCheck, MapPin, Plus, Settings2 } from "lucide-react";
 
 export function TripDetailsPage() {
   return (
@@ -24,7 +24,7 @@ export function TripDetailsPage() {
         </div>
       </header>
 
-      <main className="flex gap-16">
+      <main className="flex gap-16 px-6">
         <div className="flex-1 space-y-6">
           <div className="flex items-center justify-between">
             <h2 className="text-3xl font-semibold">Atividades</h2>
@@ -41,6 +41,27 @@ export function TripDetailsPage() {
                 <span className="text-xs text-zinc-500">Sábado</span>
               </div>
               <p className="text-zinc-500 text-sm">Nenhuma atividade cadastrada nessa data.</p>
+            </div>
+
+            <div className="space-y-2.5">
+              <div className="flex gap-2 items-baseline">
+                <span className="text-xl font-semibold text-zinc-300">Dia 18</span>
+                <span className="text-xs text-zinc-500">Domingo</span>
+              </div>
+              <div className="space-y-2.5">
+                <div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-md flex items-center gap-3 ">
+                  <CircleCheck size={20} className="text-lime-300" />
+                  <span className="text-zinc-100">Academia em grupo</span>
+                  <span className="text-zinc-400 text-sm ml-auto">08:00</span>
+                </div>
+              </div>
+              <div className="space-y-2.5">
+                <div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-md flex items-center gap-3 ">
+                  <CircleCheck size={20} className="text-lime-300" />
+                  <span className="text-zinc-100">Academia em grupo</span>
+                  <span className="text-zinc-400 text-sm ml-auto">08:00</span>
+                </div>
+              </div>
             </div>
           </div>
         </div>
