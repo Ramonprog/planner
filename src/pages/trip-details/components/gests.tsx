753498383e3c7ee66d8e582925e62a53f1b0ec6a
Ramonprog <programador.ramon@gmail.com>
2024-07-22 08:08:36 -0300

    feat: integrando area de participantes

--- src/pages/trip-details/components/gests.tsx
+++ src/pages/trip-details/components/gests.tsx
@@ -1,35 +1,33 @@
-import { CircleDashed, UserCog } from "lucide-react";
+import { CheckCircle2, CircleDashed, UserCog } from "lucide-react";
+import { useTripDataContext } from "../../../context/TripDataContext";
 
 export function Guests() {
+
+  const { tripParticipants } = useTripDataContext()
+
   return (
     <div className="space-y-6">
       <h2 className="font-semibold text-xl">Convidados</h2>
       <div className="space-y-5">
-        <div className="flex items-center justify-between gap-4">
-          <div className="space-y-1.5">
-            <span className="font-medium block text-zinc-100">Jessca Whte</span>
-            <span className="font-medium block text-zinc-500 truncate ">
-              jesscaWhte@gmal.com
-            </span>
-          </div>
-          <CircleDashed className="text-zinc-400 size-5 flex-shrink-0" />
-        </div>
+        {tripParticipants.length && tripParticipants.map(participant => {
+          return (
+            <div className="flex items-center justify-between gap-4">
+              <div className="space-y-1.5">
+                <span className="font-medium block text-zinc-100">{participant.name ? participant.name : '--'}</span>
+                <span className="font-medium block text-zinc-500 truncate ">
+                  {participant.email}
+                </span>
+              </div>
+              {participant.is_confirmed ? (
+                <CheckCircle2 className="text-lime-300 size-5 flex-shrink-0" />
+              ) : (
+                <CircleDashed className="text-zinc-400 size-5 flex-shrink-0" />
+              )}
 
-        <div className="flex items-center justify-between gap-4">
-          <div className="space-y-1.5">
-            <span className="font-medium block text-zinc-100">Ronaldo Vasconcelos</span>
-            <span className="font-medium block text-zinc-500 truncate ">vasconcelosRonalo@gmail.com</span>
-          </div>
-          <CircleDashed className="text-zinc-400 size-5 flex-shrink-0" />
-        </div>
+            </div>
+          )
+        })}
 
-        <div className="flex items-center justify-between gap-4">
-          <div className="space-y-1.5">
-            <span className="font-medium block text-zinc-100">Claudo Marques</span>
-            <span className="font-medium block text-zinc-500 truncate ">marquesClaudio@gmail.com</span>
-          </div>
-          <CircleDashed className="text-zinc-400 size-5 flex-shrink-0" />
-        </div>
       </div>
       <button className="bg-zinc-800 w-full justify-center text-zinc-200 rounded-lg py-2 px-5 font-medium flex items-center gap-2 hover:bg-zinc-700">
         Gerencar convidados
